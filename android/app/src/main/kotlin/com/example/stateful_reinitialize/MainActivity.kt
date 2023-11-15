ba4e9925a1e470d06557cfa020c513a8e4db0f74
icodingchef <icodingchef@gmail.com>
2023-11-15 10:18:51 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/stateful_reinitialize/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stateful_reinitialize
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorSchemeSeed: Colors.blue),
+      home: const ParentWidget(),
+    );
+  }
+}
+
+class ParentWidget extends StatefulWidget {
+  const ParentWidget({super.key});
+
+  @override
+  State<ParentWidget> createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  int data = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text(
+          'Rebuilding Stateful Widget',
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            DataDisplay(data2: data),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  data += 1;
+                });
+              },
+              child: const Text('Update Data'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DataDisplay extends StatefulWidget {
+  final int data2;
+
+  const DataDisplay({super.key, required this.data2});
+
+  @override
+  State<DataDisplay> createState() => _DataDisplayState();
+}
+
+class _DataDisplayState extends State<DataDisplay> {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Received Data: ${widget.data2}',
+      style: const TextStyle(
+        fontSize: 25,
+        color: Colors.red,
+      ),
+    );
+  }
+}
