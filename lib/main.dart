e4727705aecbaf504cfb5b9fa2625d09fd94bf1c
icodingchef <icodingchef@gmail.com>
2023-12-12 17:36:25 +0900

    Revised

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(colorSchemeSeed: Colors.blue),
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+      ),
       home: const ParentWidget(),
     );
   }
